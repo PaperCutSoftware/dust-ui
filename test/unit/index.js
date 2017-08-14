d5a6d9a55c7871337825d29abf4017a191bb1f71
Phil Brown <philip.brown@papercut.com>
2017-08-14 15:17:41 +1000

    Added Travis CI config to run unit tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -5,4 +5,3 @@ Vue.config.productionTip = false
 // require all test files (files that ends with .spec.js)
 const testsContext = require.context('./specs', true, /\.spec$/)
 testsContext.keys().forEach(testsContext)
-
